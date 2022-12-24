ded2a58f37d3a5fd2c1e8d4a41630ea64e88bfcd
Hani Amer <cvba2222@gmail.com>
2022-12-24 21:18:53 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, nameOfDriver) {
+    const result = drivers.filter(name => name.toUpperCase() === nameOfDriver.toUpperCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, nameOfDriver) {
+    const result = drivers.filter(name => name.slice(0, 1) === nameOfDriver.slice(0, 1))
+    return result;
+}
+
+function matchName(drivers, nameOfDriver) {
+    const result = drivers.filter(n => n.name === nameOfDriver)
+    return result;
+}
